6cdbdf49248e2784d6b039f56a12bacb246c8415
Hzzzp <143874691+Hzzzp@users.noreply.github.com>
2023-10-25 17:00:25 +0800

    Add files via upload

---
+++ Ricker_my.c
@@ -0,0 +1,34 @@
+/*
+ * @Author: Hzzzp
+ * @Date: 2023-10-25
+ * @email: hzzz507655@gmail.com
+ * @website-github: https://github.com/Hzzzp
+ * @LastEditTime: 2023-10-25
+ */
+#include <stdio.h>
+#include <math.h>
+
+double Ricker(double f,double t){
+    double pi = M_PI;   //定义变量pi，存储圆周率（π）的值
+    double term1 = 1-2*pi*pi*f*f*t*t;
+    double term2 = exp(-pi*pi*f*f*t*t);
+    return term1*term2;
+}
+
+int main()
+{
+    double f = 10.0;         //中心频率
+    int numPoints = 101;     //时间点数量
+    double timeStep = 0.01;  //时间步长
+    double t = 0.0;         //初始时间
+    int i;
+
+    for (i = 0;i < numPoints; i++){
+
+    double result = Ricker(f, t);
+    printf("Ricker Wavelet at t=%f: %f\n", t, result);
+    t += timeStep;  //更新时间节点
+    }
+
+    return 0;
+}
